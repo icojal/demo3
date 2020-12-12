5c63f5630957b3a5539e09a90f9d2856ad25ffcd
icojal <75904583+icojal@users.noreply.github.com>
2020-12-12 17:18:00 -0500

    Update cos.tf

--- cos.tf
+++ cos.tf
@@ -9,10 +9,10 @@ resource ibm_resource_instance cos {
   location          = "global" 
   resource_group_id = "${data.ibm_resource_group.resource_group.id}"
 
-  parameters = {
-    service-endpoints = "${var.end_points}"
+  #parameters = {
+  #  service-endpoints = "${var.end_points}"
     #key_protect_key   = "${ibm_kp_key.root_key.crn}"
-  }
+  #}
   
   tags = "${var.tags}"
 
